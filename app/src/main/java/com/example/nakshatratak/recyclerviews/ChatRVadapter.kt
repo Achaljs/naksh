3dee189cad456a1bd034aad40a818c291c2b0c09
achal <skj7228327@gmail.com>
2024-07-23 22:48:53 +0530

    ok

---
+++ app/src/main/java/com/example/nakshatratak/recyclerviews/ChatRVadapter.kt
@@ -0,0 +1,56 @@
+package com.example.nakshatratak.recyclerviews
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nakshatratak.MainActivity
+import com.example.nakshatratak.Profile
+import com.example.nakshatratak.R
+import com.google.android.material.button.MaterialButton
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ChatRVadapter(private val context: Context?, private val dataList: List<DataModel>) :
+    RecyclerView.Adapter<ChatRVadapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.chatfragmentlayout, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = dataList[position]
+
+        holder.bind(data)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: CircleImageView = itemView.findViewById(R.id.profileimage)
+
+        private val button:MaterialButton = itemView.findViewById(R.id.button2)
+
+        fun bind(data: DataModel) {
+            imageView.setImageResource(data.image)
+
+
+            imageView.setOnClickListener {
+
+                context?.startActivity(Intent(context,Profile::class.java))
+
+            }
+
+
+
+        }
+    }
+
+//
+}
