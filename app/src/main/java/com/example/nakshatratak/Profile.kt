241f33b1d6fbee229746eb44f15ea050520d68f8
achal Jaiswal <skj7228327@gmail.com>
2024-07-23 18:06:10 +0530

    added

--- app/src/main/java/com/example/nakshatratak/Profile.kt
+++ app/src/main/java/com/example/nakshatratak/Profile.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.graphics.RenderEffect
 import android.graphics.RenderEffect.createShaderEffect
 import android.graphics.Shader
+import android.graphics.Typeface
 import android.os.Build
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.WindowManager
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -39,15 +42,27 @@ class Profile : AppCompatActivity() {
         val collapsingToolbar: CollapsingToolbarLayout = findViewById(R.id.collapsingToolbar)
         collapsingToolbar.title = "Aditya Dhar Dwivedi"
 
+        collapsingToolbar.setCollapsedTitleTypeface(Typeface.DEFAULT_BOLD);
+      //  collapsingToolbar.setCollapsedTitleTypeface(Typeface.DEFAULT_BOLD);
+
         val image:ImageView = findViewById(R.id.profile)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             image.setRenderEffect(
                 RenderEffect.createBlurEffect(
-                    20.0f, 20.0f, Shader.TileMode.MIRROR
+                    95.0f, 95.0f, Shader.TileMode.MIRROR
                 )
             )
         }
 
 
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()  // Or handle your custom logic here
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
